95c975883b21d8ad0b2f6e130910421daae95b2e
Partonetrain <Partonetrain@gmail.com>
2024-07-07 20:11:32 -0500

    fix #1

--- src/main/java/info/partonetrain/customrarity/Customrarity.java
+++ src/main/java/info/partonetrain/customrarity/Customrarity.java
@@ -3,19 +3,18 @@ package info.partonetrain.customrarity;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Customrarity implements ModInitializer {
 
-	public static final String MOD_ID = "customrarity";
+    public static final String MOD_ID = "customrarity";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static CustomrarityConfig config;
+    public static CustomrarityConfig config;
 
-	@Override
-	public void onInitialize() {
-		AutoConfig.register(CustomrarityConfig.class, JanksonConfigSerializer::new);
-		config = AutoConfig.getConfigHolder(CustomrarityConfig.class).getConfig();
-	}
+    @Override
+    public void onInitialize() {
+        AutoConfig.register(CustomrarityConfig.class, JanksonConfigSerializer::new);
+        config = AutoConfig.getConfigHolder(CustomrarityConfig.class).getConfig();
+    }
 }
--- src/main/java/info/partonetrain/customrarity/mixin/ItemMixin.java
+++ src/main/java/info/partonetrain/customrarity/mixin/ItemMixin.java
@@ -1,10 +1,7 @@
 package info.partonetrain.customrarity.mixin;
 
 import info.partonetrain.customrarity.Customrarity;
-import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -18,35 +15,34 @@ import java.util.Arrays;
 
 @Mixin(Item.class)
 public class ItemMixin {
-	@Inject(at = @At("HEAD"), method = "getRarity", cancellable = true)
-	private void init(ItemStack itemStack, CallbackInfoReturnable<Rarity> cir) {
-		//Customrarity.LOGGER.info("getRarity called");
-		String tag = itemStack.getOrCreateTag().getString("CustomRarity");
-		//Customrarity.LOGGER.info(tag);
-		if(!tag.isEmpty()){
-            switch (tag) {
-                case "Common" -> cir.setReturnValue(Rarity.COMMON);
-                case "Uncommon" -> cir.setReturnValue(Rarity.UNCOMMON);
-                case "Rare" -> cir.setReturnValue(Rarity.RARE);
-                case "Epic" -> cir.setReturnValue(Rarity.EPIC);
-				default -> cir.setReturnValue(Rarity.COMMON);
+    @Inject(at = @At("HEAD"), method = "getRarity", cancellable = true)
+    private void customrarity_changeRarity(ItemStack itemStack, CallbackInfoReturnable<Rarity> cir) {
+        //Customrarity.LOGGER.info("getRarity called");
+        if(itemStack.getTag() != null) {
+            String tag = itemStack.getTag().getString("CustomRarity");
+            //Customrarity.LOGGER.info(tag);
+            if (!tag.isEmpty()) {
+                switch (tag) {
+                    case "Common" -> cir.setReturnValue(Rarity.COMMON);
+                    case "Uncommon" -> cir.setReturnValue(Rarity.UNCOMMON);
+                    case "Rare" -> cir.setReturnValue(Rarity.RARE);
+                    case "Epic" -> cir.setReturnValue(Rarity.EPIC);
+                    default -> cir.setReturnValue(Rarity.COMMON);
+                }
             }
-		}
+        }
 
-		if(cir.getReturnValue() == null){
-			ResourceLocation itemID = BuiltInRegistries.ITEM.getKey(itemStack.getItem());
-			if(Arrays.asList(Customrarity.config.commons).contains(itemID.toString())){
-				cir.setReturnValue(Rarity.COMMON);
-			}
-			else if(Arrays.asList(Customrarity.config.uncommons).contains(itemID.toString())){
-				cir.setReturnValue(Rarity.UNCOMMON);
-			}
-			else if(Arrays.asList(Customrarity.config.rares).contains(itemID.toString())){
-				cir.setReturnValue(Rarity.RARE);
-			}
-			else if(Arrays.asList(Customrarity.config.epics).contains(itemID.toString())){
-				cir.setReturnValue(Rarity.EPIC);
-			}
-		}
-	}
+        if (cir.getReturnValue() == null) {
+            ResourceLocation itemID = BuiltInRegistries.ITEM.getKey(itemStack.getItem());
+            if (Arrays.asList(Customrarity.config.commons).contains(itemID.toString())) {
+                cir.setReturnValue(Rarity.COMMON);
+            } else if (Arrays.asList(Customrarity.config.uncommons).contains(itemID.toString())) {
+                cir.setReturnValue(Rarity.UNCOMMON);
+            } else if (Arrays.asList(Customrarity.config.rares).contains(itemID.toString())) {
+                cir.setReturnValue(Rarity.RARE);
+            } else if (Arrays.asList(Customrarity.config.epics).contains(itemID.toString())) {
+                cir.setReturnValue(Rarity.EPIC);
+            }
+        }
+    }
 }
